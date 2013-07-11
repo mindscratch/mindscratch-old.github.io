470043110c84cea50ba1de4a406745cde3589f8f
Craig Wickesser <craig@mindscratch.org>
2013-07-10 20:53:57 -0400

    change header image of theme, add script to simplify creating a post

---
+++ bin/post.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Path to your octopress installation
+OCTOPATH=`pwd`
+#Editor to open the new markdown post in
+EDITORAPP=Mou
+
+if [[ -s "$HOME/.rvm/scripts/rvm" ]] ; then
+  source "$HOME/.rvm/scripts/rvm"
+fi
+
+#Exporting encoding
+export 'LC_CTYPE=en_US.UTF-8'
+
+cd $OCTOPATH
+rvm use 1.9.3@octopress
+OCTOPOST=$(rake new_post['{query}']| grep -o 'source/_posts/.*')
+open -a "$EDITORAPP" $OCTOPATH/$OCTOPOST
